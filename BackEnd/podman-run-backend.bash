c241d1a0c759de39858d2e646d434871bffd66b0
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2026-01-26 23:08:11 +0100

    Update echo command to new GraphQL schema URL

--- BackEnd/podman-run-backend.bash
+++ BackEnd/podman-run-backend.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm --file Containerfile --tag quarkus:backend .
 podman run --interactive --tty --publish 8888:8888 quarkus:backend
-echo "browse https://localhost:8888/graphiql"
+echo "browse http://localhost:8888/graphql/schema.graphql"
